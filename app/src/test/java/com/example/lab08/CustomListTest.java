e93322e0e36f6fe66a2761212b93dde16d7614e5
Graeme McIvor <gmmcivor@ualberta.ca>
2025-10-28 14:42:22 -0600

    Added failing test for hasCity() method

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -1,7 +1,31 @@
 package com.example.lab08;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class CustomListTest {
 
+    private CustomList cityList;
+
+    @Before
+    public void setUp() {
+        cityList = new CustomList();
+    }
+
+    @Test
+    public void testHasCity() {
+        City edmonton = new City("Edmonton", "AB");
+        City calgary = new City("Calgary", "AB");
+
+        cityList.addCity(edmonton);
+
+        // Expect true for city that was added
+        assertTrue(cityList.hasCity(edmonton));
+
+        // Expect false for city that was not added
+        assertFalse(cityList.hasCity(calgary));
+    }
 }
